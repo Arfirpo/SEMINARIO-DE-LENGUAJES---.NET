6248f185114c4de8342a7a500a1e56239410859c
Agustin Firpo <agustin_@outlook.com.ar>
2026-03-30 17:20:31 -0300

    creando repositorio del proyecto .NET - seminario de lenguajes - UNLP

---
+++ proyectosDotnet/teoria1/Program.cs
@@ -0,0 +1,5 @@
+﻿int x = 10;
+Console.WriteLine(x++ == 10);
+Console.WriteLine(x-- == 10);
+Console.WriteLine(++x == 10);
+Console.WriteLine(--x == 10);
---
+++ proyectosDotnet/teoria2/Program.cs
@@ -0,0 +1,7 @@
+﻿//object obj = 7.3; // obj apunta a un valor de tipo double
+//Console.WriteLine(obj.GetType);
+//obj = "Casa"; // ahora de tipo string
+//Console.WriteLine(obj.GetType);
+
+Object obj = (char) ('A' +  1); // ahora de tipo int
+Console.WriteLine(obj);
